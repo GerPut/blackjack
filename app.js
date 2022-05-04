d68133e26f4c2ce85909c778ec8372e9563aa14e
Gerhard <gerhard.putter@yahoo.com>
2022-05-04 09:53:00 +0200

    Set up start game function.

---
+++ app.js
@@ -0,0 +1,20 @@
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Woohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.innerText = message
+}
